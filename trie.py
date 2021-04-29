13c335bee0fcae0835fd2233eb336e37f55f7564
Katie Greed <kgreed@nd.edu>
2021-04-28 21:57:43 -0400

    Brute Force

--- trie.py
+++ trie.py
@@ -58,6 +58,19 @@ def is_word(root, word: str) -> bool:
 	else:
 		return False
 
+def brute_force1(word: str):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L,R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L,R in splits if len(R)>1]
+    replaces = [L + c + R[1:] for L,R in splits if R for c in alphabet]
+    inserts = [L + c + R for L,R in splits for c in alphabet]
+    return set(deletes + transposes + replaces + inserts)
+
+def brute_force2(word: str):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return (edits for i in brute_force1(word) for edits in brute_force1(i))
+
 def main():
     root = TrieNode('*')
     insert(root, "butterfly")
