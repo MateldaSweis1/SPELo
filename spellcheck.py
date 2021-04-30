592e21d5c55218ca9dfccea1a32cea5fab0e6119
Jesus Robles <jrobles2@nd.edu>
2021-04-30 14:00:49 -0400

    added finding suggestions using prefix

--- spellcheck.py
+++ spellcheck.py
@@ -6,7 +6,7 @@ import trie
 root = trie.TrieNode('*')
 
 # loop through lines(words) in a file and store in trie
-with open("testdict.txt","r") as afile:
+with open("studentMachineDict.txt","r") as afile:
 	for line in afile:
 		word=line.strip()
 		# add word to trie
@@ -28,18 +28,40 @@ with open("userin.txt","r") as afile:
 	
 # TODO spell check algorithms
 for word in words:
-	isWord = trie.is_word(root, word)
+	isWord = trie.is_word(root, word.lower())
 	print(f"{word} {isWord}")
 
 for word in words:
-	if not trie.is_word(root, word):
-		for index in range(len(word)):
-			isPrefix = trie.find_prefix(root, word[:len(word)-index])
-			if isPrefix[0]:
-				print(word[:len(word)-index])
-				#suggestions = trie.find_quick_suggestion(root, word, len(word))
-				#print(suggestions)	
 
+	if not trie.is_word(root, word.lower()):
+		for index in range(len(word)):
+			prefix = word[:len(word)-index]
+			isPrefix = trie.find_prefix(root, prefix)
+			if isPrefix[0] and len(prefix) > 2:
+
+				print(word)
+				print()
+				print(prefix)
+
+				suggestions = []
+				node = trie.find_prefix_node(root, prefix)
+				trie.in_order_print(node, suggestions, prefix, word)
+
+				print(suggestions)
+				print()
+
+				#reverse = []
+				#trie.reverse_print(node,reverse, prefix, word)
+				#print(reverse)
+				#print()
+
+				temp = ""
+				suffix = []
+				trie.reverse_prefix(node,suffix, prefix,temp, word)
+				print(suffix)
+				print()
+
+	
 # TODO run through sentences
 
 # TODO grammar check algorithm
