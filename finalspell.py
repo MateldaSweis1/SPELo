0b02c5adc38b7c3a9e5b4eb11a2ee58203e26191
Katie Greed <kgreed@nd.edu>
2021-05-10 21:17:27 -0400

    Main Function

--- finalspell.py
+++ finalspell.py
@@ -5,175 +5,122 @@ import trie
 # initialize trie
 root = trie.TrieNode('*')
 
-# loop through lines(words) in a file and store in trie
-with open("studentMachineDict.txt","r") as afile:
-	for line in afile:
-		word=line.rstrip()
-		# add word to trie
-		trie.insert(root, word)
-		
-commonWords = {}
-with open("commonWords2.txt", "r") as bfile:
-    for index, word in enumerate(bfile): 
-        commonWords[word.strip()] = index
+def main():
 
-#print(commonWords)
+# loop through lines(words) in a file and store in trie
+    with open("studentMachineDict.txt","r") as afile:
+            for line in afile:
+                    word=line.rstrip()
+                    # add word to trie
+                    trie.insert(root, word)
+                    
+    commonWords = {}
+    with open("commonWords2.txt", "r") as bfile:
+        for index, word in enumerate(bfile): 
+            commonWords[word.strip()] = index
+
 
 # TODO recieve user input
 
 # save words as a list without punctuation
-#punc = '''!()-[]{};:'"\,`<>./?@#$%^&*_~'''
-wackypunc = '[]{}()`<>\@#^_~'
-goodpunc = '\'!-;:"|,\.?$%&*+=/'
-words = []
+    wackypunc = '[]{}()`<>\@#^_~'
+    goodpunc = '\'!-;:"|,\.?$%&*+=/'
+    words = []
 
-with open("test1.txt","r") as afile:
-    for text in afile:
-        for let in text:
-            if let in wackypunc:
-                text = text.replace(let,"")
-        text = text.split()
-        for word in text:
-            for letter in word:
-                if letter in goodpunc:
-                    word = word.translate({ord(letter): None})
-                if word == "":
-                    continue
-            
-                addPunc = []
-                if letter in goodpunc:
-                    addPunc.append(letter)
-            words.append(word)
-            for punc in addPunc:
-                words.append(punc)
-'''
-            words = text.split()
-            for phrase in words:
-                for letter in phrase:
+    with open("test1.txt","r") as afile:
+        for text in afile:
+            for let in text:
+                if let in wackypunc:
+                    text = text.replace(let,"")
+            text = text.split()
+            for word in text:
+                for letter in word:
                     if letter in goodpunc:
-                        phrase = phrase.translate({ord(letter): None})
-                if phrase == "":
-                    continue
-                grammarlist.append(phrase)
-                if letter in goodpunc:
-                    grammarlist.append(letter)
+                        word = word.translate({ord(letter): None})
+                    if word == "":
+                        continue
+                
+                    addPunc = []
+                    if letter in goodpunc:
+                        addPunc.append(letter)
+                words.append(word)
+                for punc in addPunc:
+                    words.append(punc)
 
-print(grammarlist)
-'''
-	
+            
 # TODO spell check algorithms
-for word in words:
-	isWord = trie.is_word(root, word.lower())
-	print(f"{word} {isWord}")
+    final_list = []
+    for i,word in enumerate(words):
+        brutelist = []
+        if not trie.is_word(root, word.lower()):
+            brutelist = trie.brute_force1(root, word)
+            brutelist = set(brutelist)
 
-final_list = []
-for i,word in enumerate(words):
-    brutelist = []
-    if not trie.is_word(root, word.lower()):
-        brutelist = trie.brute_force1(root, word)
-        brutelist = set(brutelist)
-
-        found = False
-        for index in range(len(word)):
-            prefix = word[:len(word)-index]
-            isPrefix = trie.find_prefix(root, prefix)
-            if isPrefix[0] and len(prefix) > 2:
-                
-                suggestions = []
-                node = trie.find_prefix_node(root, prefix)
-                trie.in_order_print(node, suggestions, prefix, word)
-                
-                #Compare
-                '''
-                bestOption=""
-                closeness = 5
-                for brute in brutelist:
-                    if brute in suggestions:
-                        final_list.append(brute) 
-                        found = True
-                        if closeness >= abs(len(word) - len(brute)): 
-                            bestOption = brute
-                            closeness = abs(len(word) - len(brute))
-
-                '''
-                bestOption=""
-                lowestRank = 10000 
-                for brute in brutelist:
-                    if brute in commonWords:
-                        final_list.append(brute)  
-                        found = True
-                        tempRank = commonWords[brute]
-                        if tempRank < lowestRank: 
-                            bestOption = brute 
-
-                if bestOption:
-                    words[i] = bestOption
+            found = False
+            for index in range(len(word)):
+                prefix = word[:len(word)-index]
+                isPrefix = trie.find_prefix(root, prefix)
+                if isPrefix[0] and len(prefix) > 2:
                     
-                if found:
-                    break
-
-
-                bestOption=""
-                closeness = 5
-                for brute in brutelist:
-                    if brute in suggestions:
-                        final_list.append(brute) 
-                        found = True
-                        if closeness >= abs(len(word) - len(brute)): 
-                            bestOption = brute
-                            closeness = abs(len(word) - len(brute))
-
-                if bestOption:
-                    words[i] = bestOption 
-
-                if found:
-                    break
+                    suggestions = []
+                    node = trie.find_prefix_node(root, prefix)
+                    trie.in_order_print(node, suggestions, prefix, word)
                     
-                if not found and len(prefix) == 3:
-                    print(word)
-                    suffix = []
-                    temp = ""
-                    trie.reverse_prefix(node,suffix, prefix,temp, word)
+                    #Compare
+                    bestOption=""
+                    lowestRank = 10000 
+                    for brute in brutelist:
+                        if brute in commonWords:
+                            final_list.append(brute)  
+                            found = True
+                            tempRank = commonWords[brute]
+                            if tempRank < lowestRank: 
+                                bestOption = brute 
 
-                    maxSuffix = ""
-                    for suff in suffix:
-                        if len(suff) > len(maxSuffix):
-                            maxSuffix = suff
-
-                    print(maxSuffix)
-
-                    for sugg in suggestions:
-                        if maxSuffix in sugg: 
-                            words[i] = sugg
+                    if bestOption:
+                        words[i] = bestOption
+                        
+                    if found:
+                        break
 
 
-                    
-                 
+                    bestOption=""
+                    closeness = 5
+                    for brute in brutelist:
+                        if brute in suggestions:
+                            final_list.append(brute) 
+                            found = True
+                            if closeness >= abs(len(word) - len(brute)): 
+                                bestOption = brute
+                                closeness = abs(len(word) - len(brute))
 
-				#print(suggestions)
-				#print()
+                    if bestOption:
+                        words[i] = bestOption 
 
-				#reverse = []
-				#trie.reverse_print(node,reverse, prefix, word)
-				#print(reverse)
-				#print()
+                    if found:
+                        break
+                        
+                    if not found and len(prefix) == 3:
+                        suffix = []
+                        temp = ""
+                        trie.reverse_prefix(node,suffix, prefix,temp, word)
 
-		#temp = ""
-		#suffix = []
-		#trie.reverse_prefix(node,suffix, prefix,temp, word)
-				#print(suffix)
-				#print()
-                                
-        #print(word)
-				#print()
-				#print(prefix)
-         
-print(final_list)   
-for word in words:
-    print(f'{word} ', end='')     
-  	
-# TODO run through sentences
+                        maxSuffix = ""
+                        for suff in suffix:
+                            if len(suff) > len(maxSuffix):
+                                maxSuffix = suff
 
-# TODO grammar check algorithm
+                        for sugg in suggestions:
+                            if maxSuffix in sugg: 
+                                words[i] = sugg
+
+               
+    #for word in words:
+    #    print(f'{word} ', end='')     
+    
+    return words
+
+if __name__ == "__main__":
+    main()
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
