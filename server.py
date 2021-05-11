7edb05c282f981e68c672b733e16c33aa8dd624b
Marcelo Carrasquillo <mcarras2@nd.edu>
2021-05-11 13:12:51 -0400

    Final aesthetic tweaks

--- server.py
+++ server.py
@@ -2,21 +2,10 @@ from flask import Flask, request, render_template
 import finalspell
 app = Flask(__name__)
 
-numtest = 5
-
-@app.route("/script", methods=['GET','POST'])
-def script():
-    input_string = request.form['data']
-
-    input_string = upper(input_string)
-
-    return f"<h1>{input_string}<\h1>"
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    global numtest
     if request.method=="GET":
-        numtest = numtest +1
         return render_template('index.html',text = '')
     else:
         user_input = request.form["ogtext"]
